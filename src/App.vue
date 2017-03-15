362443dc5ee19de0a4b22e35aefeff780698cbcd
koko <frontend_koko@yeah.net>
2017-03-15 23:27:25 +0800

    init 0315

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <el-button @click.native="startHacking">Let's do it</el-button>-->
+    <side-nav></side-nav>
+  </div>
+</template>
+
+<script>
+import sideNav from '../src/component/sideNav.vue'
+
+export default {
+  data () {
+    return {
+      msg: 'Use Vue 2.0 Today!'
+    }
+  },
+
+  methods: {
+    startHacking () {
+      this.$notify({
+        title: 'It Works',
+        message: 'We have laid the groundwork for you. Now it\'s your time to build something epic!',
+        duration: 6000
+      })
+    }
+  },
+  components: {
+    sideNav
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+ .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+</style>
