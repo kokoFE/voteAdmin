f4ed4589dca4f94b16e02b39308920d0db2e7cf3
kokoFE <frontend_koko@yeah.net>
2017-04-21 17:21:48 +0800

    0421

--- src/component/addVote.vue
+++ src/component/addVote.vue
@@ -1,19 +1,26 @@
 <template>
   <el-card class="box-card" style="min-height: 400px;">
     <h2 id="vote-title" @dblclick="edit" v-text="vote.title"></h2>
-    <el-input v-model="vote.title" :class="{visable: editing, none: !editing}"></el-input>
+    <!--<el-input v-model="vote.title" :class="{visable: editing, none: !editing}"></el-input>-->
+    <!--<el-input v-model="vote.title"></el-input>-->
     <el-row v-if="vote.title.length">
       <el-col>
         <ol>
           <li v-for="(item, index) in vote.list">
-            <h3 @dblclick="edit(item)" :class="{none: item.editing}">{{item.title}}</h3>
-            <el-input @blur="save(item)" v-model="item.title" :class="{visable: item.editing, none: !editing}"></el-input>
+            <h3 @dblclick="edit(item)" :class="{none: editing == item  }">{{item.title}}</h3>
+            <el-input @blur="save(item)" v-model="item.title" :class="{visable: editing == item, none: editing != item}"  class="input"></el-input>
             <div>
-              <el-input v-if="item.cate == 'text'" disabled placeholder="请输入文字..."></el-input>
+              <el-input v-if="item.cate == 'text'" disabled placeholder="请输入文字..."  class="input"></el-input>
 
               <div  v-else-if="item.cate == 'radio'"  v-for="(option, index) in item.options" style="margin-bottom:10px">
-                <el-radio @dblclick="save(item)" :label="option" :key="index" class="item"></el-radio>
-                <el-input v-model="item.options[index]"  class="input"></el-input>
+                <!--<el-radio @change="test(item)" :label="option" :key="index" class="item"></el-radio>-->
+                <label class="el-radio item" @dblclick="edit(option)">
+                  <span class="el-radio__input">
+                    <span class="el-radio__inner"></span>
+                  </span>
+                  <span class="el-radio__label" :class="{none: editing == option}">{{ option.option }}</span>
+                </label>
+                <el-input @blur="save(option)" v-model="item.options[index].option" class="input" :class="{visable: editing == option, none: editing != option}"></el-input>
                 <el-button-group>
                   <el-button @click="move(item.options,index,-1)" icon="arrow-up" size="mini" type="default" :disabled="index == 0"></el-button>
                   <el-button @click="move(item.options,index,1)" icon="arrow-down" size="mini" type="default" :disabled="index == item.options.length - 1"></el-button>
@@ -22,18 +29,23 @@
               </div>
               
               <div v-else-if="item.cate == 'checkbox'" v-for="(option, index) in item.options">
-                <el-checkbox  @dblclick="edit(item)"   :label="option" :key="index"></el-checkbox>
-                <el-input @blur="save(item)" v-model="item.options[index]"  class="input"></el-input>
+                <!--<el-checkbox  @change="test(item)"   :label="option" :key="index"></el-checkbox>-->
+                <label class="el-checkbox" @dblclick="edit(option)">
+                  <span class="el-checkbox__input">
+                    <span class="el-checkbox__inner"></span>
+                  </span><span class="el-checkbox__label" :class="{none: editing == option}">{{  option.option  }}</span>
+                </label>
+                <el-input @blur="save(option)" v-model="item.options[index].option" class="input" :class="{visable: editing == option, none: editing != option}"></el-input>
                 <el-button-group>
                   <el-button @click="move(item.options,index,-1)" icon="arrow-up" size="mini" type="default" :disabled="index == 0"></el-button>
                   <el-button @click="move(item.options,index,1)" icon="arrow-down" size="mini" type="default" :disabled="index == item.options.length - 1"></el-button>
-                  <el-button @click="item.options.splice(index,1)" icon="close" size="mini" type="default"></el-button>
+                  <el-button @click="item.options.splice(index,1)" icon="close" size="mini" type="default" :disabled="item.options.length == 1"></el-button>
                 </el-button-group>
               </div>
             </div>
             <el-row>
               <el-col>
-                <el-button @click="addOption(item)" size="small" type="info">新增选项</el-button>
+                <el-button @click="addOption(item)" size="small" type="info" v-if="['radio','checkbox'].indexOf(item.cate) > -1">新增选项</el-button>
                 <el-button @click="move(vote.list,index,-1)" size="small" :plain="true" :disabled="index == 0">上移</el-button>
                 <el-button @click="move(vote.list,index,1)" size="small" :plain="true" :disabled="index == vote.list.length -1">下移</el-button>
                 <el-button @click="vote.list.splice(index,1)"size="small" type="danger">删除</el-button>
@@ -62,7 +74,7 @@
           :picker-options="pickerOptions">
         </el-date-picker>
       </el-col>
-      <el-button type="primary" @click="save">保存问卷</el-button>
+      <el-button type="primary" @click="publish">保存问卷</el-button>
       <el-button type="success">发布问卷</el-button>
     </el-row>
   </el-card>
@@ -78,7 +90,7 @@
         },
         setDate: '',
         dialogTableVisible: false,
-        editing: false,
+        editing: null,
         vote: {
           title: '请输入问卷名称',
           list: []
@@ -87,29 +99,46 @@
     },
     methods: {
       save(item) {
-        item.editing = false;
-        console.log(item.editing)
+        // item.editing = false;
+        this.editing = null;
+      },
+      publish() {
+        console.log(this.vote)
+      },
+      test(item) {
+        console.log(item)
+        console.log(this)
       },
       edit(item) {
-        item.editing = true;
-        console.log(item.editing )
+        this.editing = item;
+        console.log(item.editing)
       },
       addQuestion(cate) {
         let quest = {
           id: this.vote.list.length,
           title: '请输入问题',
           cate: cate,
-          options: ['选项一','选项二'],
-          result: [],
+          // result: [],
           editing: false
+        }
+        if (['radio','checkbox'].indexOf(cate) > -1) {
+          // quest.options = ['选项一','选项二']
+          quest.options = [
+            {"id": 0, "option": "选项一"},
+            {"id": 1, "option": "选项二"}
+          ]
         }
         this.vote.list.push(quest)
         this.dialogTableVisible = false
       },
       addOption(item) {
-        item.options.push('请输入选项')
-        // var options = this.
-        // this.vote.list.options.push('1')
+        console.log(item)
+        var data = {
+          "id":  item.options.length + 1,
+          "option": "请输入选项"
+        }
+        item.options.push(data)
+        // item.options.push("请输入选项")
       },
       move(data,index,direction) {
         console.log('move')
@@ -117,8 +146,6 @@
         var temp = data[index];
         data.splice(index,1)
         data.splice(target, 0,temp)
-        
-
       }
     }
   }
@@ -155,5 +182,10 @@ i:hover {
 }
 .visable {
   display: inline-block;
+}
+#radio {
+  width: 10px;
+  height: 10px;
+  border: 1px solid #000;
 }
 </style>
